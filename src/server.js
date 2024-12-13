757c818e3e5d5129d008af6afa76ef453fb3727a
Minu Han <mwj1205@naver.com>
2024-12-13 18:44:14 +0900

    feat: cors 정책 추가

--- src/server.js
+++ src/server.js
@@ -3,8 +3,17 @@ import express from 'express';
 import router from './routes/index.router.js';
 import errorHandlerMiddleware from './middlewares/error-handling.middleware.js';
 import { initServer } from './init/initServer.js';
+import cors from 'cors';
 
 const app = express();
+
+app.use(
+  cors({
+    origin: 'https://html-classic.itch.zone',
+    methods: 'GET, POST',
+    allowedHeaders: 'Content-Type, Authorization',
+  }),
+);
 
 app.use(express.json());
 app.use('/', router);
